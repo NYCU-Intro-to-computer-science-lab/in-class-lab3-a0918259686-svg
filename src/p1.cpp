4ecf9c303067c1a4d8b4472598b53f071df29b9f
a0918259686-svg <a0918259686@gmail.com>
2025-11-25 14:29:17 +0800

    Update p1.cpp

--- src/p1.cpp
+++ src/p1.cpp
@@ -2,9 +2,30 @@
 
 #include <iostream>
 using namespace std;
-
+int f(int n) {
+    if (n == 1) {
+        return 1;
+    }
+    else 
+    return f(n - 1)*n;
+  
+}
 int main() {
+    int n;
+    cout<<f(n)<<endl;
     
-    
+    return n*f(n-1);
+int f(int n) {
+    if (n == 1) {
+        return 1;
+    }
+    else 
+    return f(n-1) +n;
+  
+}
+int main(){
+    cout<<f(n)<<endl;
+
     return 0;
+    
 }
